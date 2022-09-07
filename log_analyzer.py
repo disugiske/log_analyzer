3cd4f5ff9796c76f5eb2f86b338d073a51501687
disugiske <disugiske@yandex.ru>
2022-09-08 01:52:51 +0300

    Change encoding for log file, add generator, add test

--- log_analyzer.py
+++ log_analyzer.py
@@ -56,9 +56,9 @@ def options_parse():
         config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
         if options.log_dir:
             logger.info(f"Передан путь к файлу config:{options.log_dir}")
-            file = open(options.log_dir, "r")
-            conf = file.read()
-            parse_config = ast.literal_eval(conf)
+            with open(options.log_dir, "r") as file:
+                conf = file.read()
+                parse_config = ast.literal_eval(conf)
             if parse_config.values() != config.values():
                 for i in parse_config:
                     config[i] = parse_config[i]
@@ -79,7 +79,7 @@ def options_parse():
         quit()
 
 
-def open_file(config):
+def find_path(config):
     data = {}
     files = []
     directory = Path(config["LOG_DIR"])
@@ -88,9 +88,7 @@ def open_file(config):
         for i in directory.iterdir():
             patch_file = re.search(r".*\.gz|.*\.txt", i.as_posix())
             if patch_file != None:
-
                 files.append(patch_file.group(0))
-
         for logs_file in files:
             data_name = re.search(r"\d\d\d\d\d\d\d\d", logs_file)
             data[logs_file] = data_name.group(0)
@@ -115,19 +113,16 @@ def open_file(config):
         quit()
 
 
-def parser(path, config):
-    result, urls = [], []
-    all_requests, all_times = 0, 0
-    count = {}
+def open_file(path):
     try:
-        if path[-2:] == "gz":
+        if path.endswith(".gz"):
             with gzip.open(path, "rt") as data_log:
-                logs = data_log.read()
-                count_str = logs.count("\n")
-        elif path[-3:] == "txt":
+                for logs in data_log:
+                    yield logs
+        elif path.endswith(".txt"):
             with open(path, "rt") as data_log:
-                logs = data_log.read()
-                count_str = logs.count("\n")
+                for logs in data_log:
+                    yield logs
         if logs == "":
             logger.info("Файл логов пустой")
             quit()
@@ -145,15 +140,26 @@ def parser(path, config):
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
         quit()
+
+
+def parser(config, logs):
+    urls = []
+    all_requests, count_str, all_times = 0, 0, 0
+    count = {}
     try:
-        first_parse = re.findall(r"(GET /\S*|POST /\S*)+ +.+ +(\d\.\d{3})", logs)
-        for i in first_parse:
-            parse = re.sub(r"GET |POST ", "", i[0])
-            urls.append(parse)
-            count.setdefault(parse, [])
-            count[parse].append(float(i[1]))
-            all_requests += 1
-            all_times += float(i[1])
+        for i in logs:
+            first_parse = re.findall(r"(GET /\S*|POST /\S*)+ +.+ +(\d\.\d{3})", i)
+            # first_parse = re.search(r"(GET /\S*|POST /\S*)", i)
+            # times = re.search(r"\s\d\.\d{3}", i)
+
+            if len(first_parse):
+                parse = re.sub(r"GET |POST ", "", first_parse[0][0])
+                urls.append(parse)
+                count.setdefault(parse, [])
+                count[parse].append(float(first_parse[0][1]))
+                all_requests += 1
+                all_times += float(first_parse[0][1])
+            count_str += 1
         logger.info("Логи распаршены")
         if all_requests != count_str:
             err_count = 100 - (all_requests / count_str) * 100
@@ -181,22 +187,22 @@ def parser(path, config):
             sorted(count.items(), key=lambda x: len(x[1]), reverse=True)
         )
         for i in itertools.islice(sorted_count, 0, int(config["REPORT_SIZE"]) - 1):
+
             urls_count = len(count[i])
             sum_time = (urls_count / all_requests) * 100
-            result.append(
-                {
-                    "url": i,
-                    "count": urls_count,
-                    "count_perc": float_cut(sum_time),
-                    "time_sum": float_cut(sum(count[i])),
-                    "time_perc": float_cut((sum_time / all_times) * 100),
-                    "time_avg": float_cut(mean(count[i])),
-                    "time_max": float_cut(max(count[i])),
-                    "time_med": float_cut(median(count[i])),
-                }
-            )
+            result = {
+                "url": i,
+                "count": urls_count,
+                "count_perc": float_cut(sum_time),
+                "time_sum": float_cut(sum(count[i])),
+                "time_perc": float_cut((sum_time / all_times) * 100),
+                "time_avg": float_cut(mean(count[i])),
+                "time_max": float_cut(max(count[i])),
+                "time_med": float_cut(median(count[i])),
+            }
+            yield result
         logger.info("Данные из лог файла преобразованны в json")
-        return result
+
     except StopIteration:
         logger.error(
             "Ошибка итерации, возможно REPORT_SIZE больше чем количество запросов"
@@ -209,12 +215,15 @@ def parser(path, config):
 
 
 def write_report(result, data_file, config):
+    list_dict = []
     try:
         with open("report.html", "r") as template_report:
             report = template_report.read()
-            template = Template(report)
-            a = template.safe_substitute(table_json=result)
-            logger.info("Открыт шаблон отчёта, данные json переданы")
+        template = Template(report)
+        for i in result:
+            list_dict.append(i)
+        template_with_data = template.safe_substitute(table_json=list_dict)
+        logger.info("Открыт шаблон отчёта, данные json переданы")
     except FileNotFoundError:
         logger.error("Не найден файл шаблона report.html")
         quit()
@@ -222,14 +231,16 @@ def write_report(result, data_file, config):
         exc_type, exc_obj, exc_tb = sys.exc_info()
         logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
         quit()
+
     try:
         os.mkdir(config["REPORT_DIR"])
         logger.info(f"Создана папка {config['REPORT_DIR']}")
     except:
         logger.info(f"Папка {config['REPORT_DIR']} уже существует")
+
     try:
         with open(f'{config["REPORT_DIR"]}/report-{data_file}.html', "w") as report_out:
-            report_out.write(a)
+            report_out.write(template_with_data)
             logger.info("Отчёт сохранён")
     except FileExistsError:
         logger.error("Ошибка создания файла отчёта")
@@ -243,8 +254,9 @@ def write_report(result, data_file, config):
 if __name__ == "__main__":
     try:
         config = options_parse()
-        path, data_file = open_file(config)
-        result = parser(path, config)
+        path, data_file = find_path(config)
+        logs = open_file(path)
+        result = parser(config, logs)
         write_report(result, data_file, config)
     except KeyboardInterrupt:
         logger.error("Операция прервана пользователем")
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import unittest
 from pathlib import Path
@@ -12,26 +13,38 @@ class FunctionalTest(unittest.TestCase):
         data = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
         self.assertEqual(out, data)
 
-    def test_open_file_gzip(self):
+    def test_find_path_gzip(self):
         config = {
             "REPORT_SIZE": 1000,
             "REPORT_DIR": "./reports",
             "LOG_DIR": "./tests/log_test",
         }
 
-        out = log_analyzer.open_file(config)
+        out = log_analyzer.find_path(config)
         data = ("tests/log_test/nginx-access-ui.log-20170830.gz", "2017.08.30")
         self.assertEqual(out, data)
 
-    def test_open_file_plain(self):
+    def test_find_path_plain(self):
         config = {
             "REPORT_SIZE": 1000,
             "REPORT_DIR": "./reports",
             "LOG_DIR": "./tests/log_plain",
         }
-        out = log_analyzer.open_file(config)
+        out = log_analyzer.find_path(config)
         data = ("tests/log_plain/nginx-access-ui.log-20170630.txt", "2017.06.30")
         self.assertEqual(out, data)
+
+    def test_open_file(self):
+        test_logs = []
+        config = {
+            "REPORT_SIZE": 1000,
+            "REPORT_DIR": "./reports",
+            "LOG_DIR": "./tests/log_plain",
+        }
+        path, data_file = log_analyzer.find_path(config)
+        logs = log_analyzer.open_file(path)
+        test_str = '1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/25019354 HTTP/1.1" 200 927 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752759" "dc7161be3" 0.390'
+        self.assertTrue(test_str in next(logs))
 
     def test_parser(self):
         config = {
@@ -39,10 +52,11 @@ class FunctionalTest(unittest.TestCase):
             "REPORT_DIR": "./reports",
             "LOG_DIR": "./tests/log_plain",
         }
-        path, date = log_analyzer.open_file(config)
-        result = log_analyzer.parser(path, config)
-        self.assertTrue(type(result) is list)
-        self.assertTrue(type(result[0]) is dict)
+        path, data_file = log_analyzer.find_path(config)
+        logs = log_analyzer.open_file(path)
+        result = log_analyzer.parser(config, logs)
+        for i in result:
+            self.assertTrue(type(i) is dict)
 
     def test_parser_result(self):
         config = {
@@ -50,8 +64,9 @@ class FunctionalTest(unittest.TestCase):
             "REPORT_DIR": "./reports",
             "LOG_DIR": "./tests/log_plain",
         }
-        path, date = log_analyzer.open_file(config)
-        result = log_analyzer.parser(path, config)
+        path, data_file = log_analyzer.find_path(config)
+        logs = log_analyzer.open_file(path)
+        result = log_analyzer.parser(config, logs)
         test_result = {
             "url": "/api/v2/banner/25019354",
             "count": 1,
@@ -62,7 +77,10 @@ class FunctionalTest(unittest.TestCase):
             "time_max": "0.390",
             "time_med": "0.390",
         }
-        self.assertTrue(test_result in result)
+        res = []
+        for i in result:
+            res.append(i)
+        self.assertTrue(test_result in res)
 
     def test_write_report(self):
         config = {
@@ -70,9 +88,10 @@ class FunctionalTest(unittest.TestCase):
             "REPORT_DIR": "./tests/reports_test",
             "LOG_DIR": "./tests/log_plain",
         }
-        path, date = log_analyzer.open_file(config)
-        result = log_analyzer.parser(path, config)
-        log_analyzer.write_report(result, date, config)
+        path, data_file = log_analyzer.find_path(config)
+        logs = log_analyzer.open_file(path)
+        result = log_analyzer.parser(config, logs)
+        log_analyzer.write_report(result, data_file, config)
         path_test = "tests/reports_test/report-2017.06.30.html"
         path = list((i.as_posix()) for i in Path("tests/reports_test").glob("*"))
         self.assertEqual(path_test, path[0])
