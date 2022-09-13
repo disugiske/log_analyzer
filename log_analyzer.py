fcc15a3aa309b4b0e47603708176af5adc010e03
disugiske <disugiske@yandex.ru>
2022-09-14 01:02:50 +0300

    Change design

--- log_analyzer.py
+++ log_analyzer.py
@@ -6,9 +6,9 @@
 #                     '$status $body_bytes_sent "$http_referer" '
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-
+import argparse
+import json
 import logging
-import ast
 import gzip
 import itertools
 import os
@@ -16,252 +16,208 @@ import re
 import sys
 from operator import itemgetter
 from string import Template
-from optparse import OptionParser
 from pathlib import Path
 from statistics import mean, median
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logging.basicConfig(
-    filename="analyzer.log",
-    encoding="UTF-8",
-    filemode="w",
-    format="[%(asctime)s] %(levelname).1s %(message)s",
-    datefmt="%d-%b-%y %H:%M:%S",
-)
-handler = logging.StreamHandler(stream=sys.stdout)
-handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname).1s %(message)s"))
-logger.addHandler(handler)
+config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
 
 
 def float_cut(obj):
     return f"{obj:.{3}f}"
 
 
-def options_parse():
-    parser = OptionParser()
-    parser.add_option(
-        "--config",
-        "-c",
-        dest="log_dir",
-        help="Укажите путь к файлу с config. По умолчанию config:"
-        '{"REPORT_SIZE": 1000,'
-        '"REPORT_DIR": "./reports",'
-        '"LOG_DIR": "./log"}',
-    )
-    (options, args) = parser.parse_args()
-    logger.info("Старт скрипта")
-    try:
-        config = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
-        if options.log_dir:
-            logger.info(f"Передан путь к файлу config:{options.log_dir}")
-            with open(options.log_dir, "r") as file:
-                conf = file.read()
-                parse_config = ast.literal_eval(conf)
-            if parse_config.values() != config.values():
-                for i in parse_config:
-                    config[i] = parse_config[i]
-            logger.info("Файл config успешно прочитан")
-            return config
-        else:
-            logger.info("Config по умолчнию")
-            return config
-    except PermissionError:
-        logger.error("Не хватает прав доступа чтобы отрыть файл config")
-        quit()
-    except FileNotFoundError:
-        logger.error("Файл не найден, неверный путь или имя файла config")
-        quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+def open_config(config_path):
+    if config_path is None:
+        return config
+    with open(config_path, "r", encoding="utf8") as conf_file:
+        parse_config = json.load(conf_file)
+    return parse_config
 
 
-def find_path(config):
+def check_config(parse_config, conf):
+    if parse_config.values() == conf.values():
+        return conf
+    else:
+        for i in parse_config:
+            conf[i] = parse_config[i]
+        return conf
+
+
+def find_patch(path_log_dir):
     data = {}
-    files = []
-    directory = Path(config["LOG_DIR"])
+    if not os.path.isdir(path_log_dir):
+        return None
+    for i in os.listdir(path_log_dir):
+        patch_file = re.search(r".*\.gz|.*\.txt", i)
+        if patch_file != None:
+            data_name = re.search(r"\d\d\d\d\d\d\d\d", patch_file.group(0))
+            data[patch_file.group(0)] = data_name.group(0)
+    file_data = sorted(data.items(), key=itemgetter(1), reverse=True)[0]
+    path_file = f"{path_log_dir}/{file_data[0]}"
+    data_file_raw = file_data[1]
+    data_file = f"{data_file_raw[:4]}.{data_file_raw[4:6]}.{data_file_raw[6:]}"
+    return path_file, data_file
 
-    try:
-        for i in directory.iterdir():
-            patch_file = re.search(r".*\.gz|.*\.txt", i.as_posix())
-            if patch_file != None:
-                files.append(patch_file.group(0))
-        for logs_file in files:
-            data_name = re.search(r"\d\d\d\d\d\d\d\d", logs_file)
-            data[logs_file] = data_name.group(0)
-        file_data = sorted(data.items(), key=itemgetter(1), reverse=True)[0]
-        path_file = file_data[0]
-        data_file_raw = file_data[1]
-        data_file = f"{data_file_raw[:4]}.{data_file_raw[4:6]}.{data_file_raw[6:]}"
 
-        for reports in Path(config["REPORT_DIR"]).glob("report-*.html"):
-            data_report = re.search(r"\d\d\d\d.\d\d.\d\d", reports.as_posix())
-            if data_report.group(0) == data_file:
-                logger.error("Отчёт на сегодня уже готов! Скрипт завершён")
-                quit(0)
-        return path_file, data_file
-
-    except FileNotFoundError:
-        logger.error("Файл логов не найден")
-        quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+def check_today_report(report_dir_patch, data_file):
+    for reports in report_dir_patch.glob("report-*.html"):
+        data_report = re.search(r"\d\d\d\d.\d\d.\d\d", reports.as_posix())
+        if data_report.group(0) == data_file:
+            return True
 
 
 def open_file(path):
-    try:
-        if path.endswith(".gz"):
-            with gzip.open(path, "rt") as data_log:
-                for logs in data_log:
-                    yield logs
-        elif path.endswith(".txt"):
-            with open(path, "rt") as data_log:
-                for logs in data_log:
-                    yield logs
-        if logs == "":
-            logger.info("Файл логов пустой")
-            quit()
-        logger.info(f"Файл {path} упешно открыт и прочитан")
-    except UnicodeEncodeError:
-        logger.error("Ошибка кодировки файла логов")
-        quit()
-    except EOFError:
-        logger.error("Неожиданный конец файла логов")
-        quit()
-    except PermissionError:
-        logger.error("Не хватает прав доступа чтобы отрыть файл логов")
-        quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+    if path.endswith(".gz"):
+        with gzip.open(path, "rt") as data_log:
+            for logs in data_log:
+                yield logs
+    elif path.endswith(".txt"):
+        with open(path, "rt") as data_log:
+            for logs in data_log:
+                yield logs
 
 
-def parser(config, logs):
+def parse_file(logs):
     urls = []
     all_requests, count_str, all_times = 0, 0, 0
     count = {}
-    try:
-        for i in logs:
-            first_parse = re.findall(r"(GET /\S*|POST /\S*)+ +.+ +(\d\.\d{3})", i)
-            # first_parse = re.search(r"(GET /\S*|POST /\S*)", i)
-            # times = re.search(r"\s\d\.\d{3}", i)
-
-            if len(first_parse):
-                parse = re.sub(r"GET |POST ", "", first_parse[0][0])
-                urls.append(parse)
-                count.setdefault(parse, [])
-                count[parse].append(float(first_parse[0][1]))
-                all_requests += 1
-                all_times += float(first_parse[0][1])
-            count_str += 1
-        logger.info("Логи распаршены")
-        if all_requests != count_str:
-            err_count = 100 - (all_requests / count_str) * 100
-            logger.error(
-                f"Не удалось распарсить {float_cut(err_count)}% логов, проверьте синтаксис"
-            )
-            if err_count > 50:
-                logger.info(
-                    "Не удалось рапарсить более 50% логов, проверьте синтаксис. Работа завершена"
-                )
-                quit()
-    except StopIteration:
-        logger.error(
-            "В файле логов изменён синтаксис, не найдены $request или $request_time"
-        )
-        quit()
-
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
-
-    try:
-        sorted_count = dict(
-            sorted(count.items(), key=lambda x: len(x[1]), reverse=True)
-        )
-        for i in itertools.islice(sorted_count, 0, int(config["REPORT_SIZE"]) - 1):
-
-            urls_count = len(count[i])
-            sum_time = (urls_count / all_requests) * 100
-            result = {
-                "url": i,
-                "count": urls_count,
-                "count_perc": float_cut(sum_time),
-                "time_sum": float_cut(sum(count[i])),
-                "time_perc": float_cut((sum_time / all_times) * 100),
-                "time_avg": float_cut(mean(count[i])),
-                "time_max": float_cut(max(count[i])),
-                "time_med": float_cut(median(count[i])),
-            }
-            yield result
-        logger.info("Данные из лог файла преобразованны в json")
-
-    except StopIteration:
-        logger.error(
-            "Ошибка итерации, возможно REPORT_SIZE больше чем количество запросов"
-        )
-        quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+    for i in logs:
+        first_parse = re.findall(r"(GET /\S*|POST /\S*)+ +.+ +(\d\.\d{3})", i)
+        if len(first_parse):
+            parse = re.sub(r"GET |POST ", "", first_parse[0][0])
+            urls.append(parse)
+            count.setdefault(parse, [])
+            count[parse].append(float(first_parse[0][1]))
+            all_requests += 1
+            all_times += float(first_parse[0][1])
+        count_str += 1
+    return all_requests, count_str, all_times, count
 
 
-def write_report(result, data_file, config):
+def check_parse(all_requests, count_str):
+    err_count = 100 - (all_requests / count_str) * 100
+    return err_count
+
+
+def make_json(all_requests, all_times, count, path_size):
+    sorted_count = dict(sorted(count.items(), key=lambda x: len(x[1]), reverse=True))
+    for i in itertools.islice(sorted_count, 0, int(path_size) - 1):
+        urls_count = len(count[i])
+        sum_time = (urls_count / all_requests) * 100
+        result = {
+            "url": i,
+            "count": urls_count,
+            "count_perc": float_cut(sum_time),
+            "time_sum": float_cut(sum(count[i])),
+            "time_perc": float_cut((sum_time / all_times) * 100),
+            "time_avg": float_cut(mean(count[i])),
+            "time_max": float_cut(max(count[i])),
+            "time_med": float_cut(median(count[i])),
+        }
+        yield result
+
+
+def make_report_dir(report_dir):
+    if not os.path.isdir(report_dir):
+        os.mkdir(report_dir)
+        return report_dir
+
+
+def open_report_template():
+    with open("report.html", "r") as template_report:
+        report = template_report.read()
+    return report
+
+
+def make_report(result, report):
     list_dict = []
-    try:
-        with open("report.html", "r") as template_report:
-            report = template_report.read()
-        template = Template(report)
-        for i in result:
-            list_dict.append(i)
-        template_with_data = template.safe_substitute(table_json=list_dict)
-        logger.info("Открыт шаблон отчёта, данные json переданы")
-    except FileNotFoundError:
-        logger.error("Не найден файл шаблона report.html")
-        quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+    template = Template(report)
+    for i in result:
+        list_dict.append(i)
+    template_with_data = template.safe_substitute(table_json=list_dict)
+    return template_with_data
 
-    try:
-        os.mkdir(config["REPORT_DIR"])
-        logger.info(f"Создана папка {config['REPORT_DIR']}")
-    except:
-        logger.info(f"Папка {config['REPORT_DIR']} уже существует")
 
-    try:
-        with open(f'{config["REPORT_DIR"]}/report-{data_file}.html', "w") as report_out:
-            report_out.write(template_with_data)
-            logger.info("Отчёт сохранён")
-    except FileExistsError:
-        logger.error("Ошибка создания файла отчёта")
+def write_report(template_with_data, data_file):
+    with open(f'{config["REPORT_DIR"]}/report-{data_file}.html', "w") as report_out:
+        report_out.write(template_with_data)
+
+
+def main(config):
+
+    path_file = find_patch(config["LOG_DIR"])
+    if path_file == None:
+        logger.error("не найдена папка с логами")
+        quit(0)
+    check_result = check_today_report(Path(config["REPORT_DIR"]), path_file[1])
+    if check_result == True:
+        logger.error("Отчёт на сегодня уже готов! Скрипт завершён")
+        return
+    logs = open_file(path_file[0])
+    if logs == "":
+        logger.info("Файл логов пустой")
+        return
+    logger.info(f"Файл {path_file[0]} упешно открыт и прочитан")
+    all_requests, count_str, all_times, count = parse_file(logs)
+    err_count = check_parse(all_requests, count_str)
+    if err_count > 50:
+        logger.error(
+            "Не удалось рапарсить более 50% логов, проверьте синтаксис. Работа завершена"
+        )
         quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
-        quit()
+    elif err_count > 0:
+        logger.info(
+            f"Не удалось распарсить {float_cut(err_count)}% логов, проверьте синтаксис"
+        )
+    else:
+        logger.info("Логи распаршены")
+    report_dir = make_report_dir(config["REPORT_DIR"])
+    if report_dir:
+        logger.info(f"Создана папка {report_dir}")
+    report = open_report_template()
+    result = make_json(all_requests, all_times, count, config_result["REPORT_SIZE"])
+    template_with_data = make_report(result, report)
+    write_report(template_with_data, path_file[1])
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        "-c",
+        help="Укажите путь к файлу с config. По умолчанию config:"
+        '{"REPORT_SIZE": 1000,'
+        '"REPORT_DIR": "./reports",'
+        '"LOG_DIR": "./log_test"}',
+    )
+    args = parser.parse_args()
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logging.basicConfig(
+        filename="analyzer.log",
+        encoding="UTF-8",
+        filemode="w",
+        format="[%(asctime)s] %(levelname).1s %(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname).1s %(message)s"))
+    logger.addHandler(handler)
+    logger.info("Старт скрипта")
+    if args.config:
+        logger.info(f"Передан путь к файлу config:{args.config}")
+    parse_config = open_config(args.config)
+    config_result = check_config(parse_config, config)
+    if config_result:
+        logger.info("Файл config успешно прочитан")
     try:
-        config = options_parse()
-        path, data_file = find_path(config)
-        logs = open_file(path)
-        result = parser(config, logs)
-        write_report(result, data_file, config)
+        main(config_result)
+        logger.info("Отчёт сохранен")
     except KeyboardInterrupt:
-        logger.error("Операция прервана пользователем")
+        logger.exception("Операция прервана пользователем", exc_info=False)
         quit()
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        logger.error(f"Ошибка: {e}, в строке {exc_tb.tb_lineno}")
+    except Exception:
+        logger.exception(f"Ошибка:")
         quit()
--- tests/test.py
+++ tests/test.py
@@ -1,101 +1,61 @@
-import itertools
+import gzip
 import os
+import runpy
+import subprocess
+import sys
+import time
 import unittest
 from pathlib import Path
-from typing import Union
 
 import log_analyzer
 
+config = {
+    "REPORT_SIZE": 1000,
+    "REPORT_DIR": "tests/reports_test",
+    "LOG_DIR": "tests/log_test",
+}
+
 
 class FunctionalTest(unittest.TestCase):
-    def test_options_parse(self):
-        out = log_analyzer.options_parse()
-        data = {"REPORT_SIZE": 1000, "REPORT_DIR": "./reports", "LOG_DIR": "./log"}
-        self.assertEqual(out, data)
+    def test_main(self):
+        sys.argv[1:] = ["-cconfig_test.txt"]
+        runpy.run_module("log_analyzer", run_name="__main__", alter_sys=True)
+        self.assertEqual(os.listdir("tests/reports_test")[0], "report-2017.08.30.html")
+        os.remove("tests/reports_test/report-2017.08.30.html")
 
-    def test_find_path_gzip(self):
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./reports",
-            "LOG_DIR": "./tests/log_test",
-        }
+    def test_main_conf(self):
+        sys.argv[1:] = ["-cconf.txt"]
+        with self.assertRaises(FileNotFoundError):
+            runpy.run_module("log_analyzer", run_name="__main__", alter_sys=True)
 
-        out = log_analyzer.find_path(config)
-        data = ("tests/log_test/nginx-access-ui.log-20170830.gz", "2017.08.30")
-        self.assertEqual(out, data)
-
-    def test_find_path_plain(self):
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./reports",
-            "LOG_DIR": "./tests/log_plain",
-        }
-        out = log_analyzer.find_path(config)
-        data = ("tests/log_plain/nginx-access-ui.log-20170630.txt", "2017.06.30")
-        self.assertEqual(out, data)
+    def test_find_patch(self):
+        path_file = log_analyzer.find_patch(config["LOG_DIR"])
+        out = ("tests/log_test/nginx-access-ui.log-20170830.gz", "2017.08.30")
+        self.assertEqual(out, path_file)
 
     def test_open_file(self):
-        test_logs = []
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./reports",
-            "LOG_DIR": "./tests/log_plain",
-        }
-        path, data_file = log_analyzer.find_path(config)
-        logs = log_analyzer.open_file(path)
-        test_str = '1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300] "GET /api/v2/banner/25019354 HTTP/1.1" 200 927 "-" "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5" "-" "1498697422-2190034393-4708-9752759" "dc7161be3" 0.390'
-        self.assertTrue(test_str in next(logs))
-
-    def test_parser(self):
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./reports",
-            "LOG_DIR": "./tests/log_plain",
-        }
-        path, data_file = log_analyzer.find_path(config)
-        logs = log_analyzer.open_file(path)
-        result = log_analyzer.parser(config, logs)
+        res_logs = []
+        logs = []
+        path_file = log_analyzer.find_patch(config["LOG_DIR"])
+        result = log_analyzer.open_file(path_file[0])
         for i in result:
-            self.assertTrue(type(i) is dict)
+            res_logs.append(i)
+        f = gzip.open(
+            "tests/log_test/nginx-access-ui.log-20170830.gz", "rt", encoding="utf-8"
+        )
+        for i in f:
+            logs.append(i)
+        self.assertTrue(res_logs[0] in logs)
+        f.close()
 
-    def test_parser_result(self):
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./reports",
-            "LOG_DIR": "./tests/log_plain",
-        }
-        path, data_file = log_analyzer.find_path(config)
-        logs = log_analyzer.open_file(path)
-        result = log_analyzer.parser(config, logs)
-        test_result = {
-            "url": "/api/v2/banner/25019354",
-            "count": 1,
-            "count_perc": "4.762",
-            "time_sum": "0.390",
-            "time_perc": "73.091",
-            "time_avg": "0.390",
-            "time_max": "0.390",
-            "time_med": "0.390",
-        }
-        res = []
-        for i in result:
-            res.append(i)
-        self.assertTrue(test_result in res)
-
-    def test_write_report(self):
-        config = {
-            "REPORT_SIZE": 1000,
-            "REPORT_DIR": "./tests/reports_test",
-            "LOG_DIR": "./tests/log_plain",
-        }
-        path, data_file = log_analyzer.find_path(config)
-        logs = log_analyzer.open_file(path)
-        result = log_analyzer.parser(config, logs)
-        log_analyzer.write_report(result, data_file, config)
-        path_test = "tests/reports_test/report-2017.06.30.html"
-        path = list((i.as_posix()) for i in Path("tests/reports_test").glob("*"))
-        self.assertEqual(path_test, path[0])
-        os.remove("tests/reports_test/report-2017.06.30.html")
+    def test_parse_file(self):
+        path_file = log_analyzer.find_patch(config["LOG_DIR"])
+        result = log_analyzer.open_file(path_file[0])
+        all_requests = log_analyzer.parse_file(result)
+        self.assertTrue(all_requests[0] == 23)
+        self.assertTrue(all_requests[1] == 23)
+        self.assertTrue(f"{all_requests[2]:.{3}f}" == "6.732")
+        self.assertTrue(all_requests[3]["/api/v2/group/1769230/banners"] == [0.628])
 
 
 if __name__ == "__main__":
